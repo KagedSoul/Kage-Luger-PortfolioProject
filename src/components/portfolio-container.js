18eacdfa3746ca5d329fb91dc5aad188bbcc0d6e
Kage Luger <wu76iyqr2f@privaterelay.appleid.com>
2021-03-04 15:07:45 -0600

    created a component example

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            // JSX
+            <div>
+                <h2>Portfolio Component Items</h2>
+            </div>
+        )
+    }
+}
