c144905118a94055d7308f8865cac391b6a3c741
Kage Luger <wu76iyqr2f@privaterelay.appleid.com>
2021-03-04 15:19:34 -0600

    functional vs state components

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import portfolioItem from './portfolio-item';
 
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    // State
+    // Lifecycle hooks
+    // Dynamic Data and react to changes = Class base = little more complex
     render() {
         return (
             // JSX
             <div>
                 <h2>Portfolio Component Items</h2>
+
+                <portfolioItem />
             </div>
         )
     }
---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function() {
+    //Functional Componet = simple, simply rendering data given to it.
+    return (
+        <div>
+            <h2>Portfolio Item go here..</h2>
+        </div>
+    )
+}
