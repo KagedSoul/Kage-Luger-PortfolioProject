202fdcd3b44645562b48ed2616a23a20afa24fb2
Kage Luger <wu76iyqr2f@privaterelay.appleid.com>
2021-03-10 14:57:52 -0600

    update nav-link styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -11,14 +11,25 @@ export default class NavigationComponent extends Component {
       <div className="nav-wrapper">
         
         <div className="left-side">
-          <NavLink exact to="/" activeClassName="active-nav-class">Home</NavLink>
-          <NavLink to="/about-me" activeClassName="active-nav-class">About</NavLink>
-          <NavLink to="/contact-me" activeClassName="active-nav-class">Contact Me</NavLink>
-          <NavLink to="/blog" activeClassName="active-nav-class">Blog</NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="active-nav-class">Home</NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="active-nav-class">About</NavLink>
+          </div>  
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact-me" activeClassName="active-nav-class">Contact Me</NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">  
+            <NavLink to="/blog" activeClassName="active-nav-class">Blog</NavLink>
+          </div>
         </div>
         <div className="right-side">Kage Luger</div>
       
-        {true ? <button>Add Blog</button> : null }
+        {/* {true ? <button>Add Blog</button> : null } */}
       </div>
     );
   }
