816b0562a98c68706911a479dfab5e0d9fc34d0e
Kage Luger <wu76iyqr2f@privaterelay.appleid.com>
2021-03-10 15:18:28 -0600

    updated current page nav-link styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -12,19 +12,19 @@ export default class NavigationComponent extends Component {
         
         <div className="left-side">
           <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="active-nav-class">Home</NavLink>
+            <NavLink exact to="/" activeClassName="active-nav-link">Home</NavLink>
           </div>
 
           <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="active-nav-class">About</NavLink>
+            <NavLink to="/about-me" activeClassName="active-nav-link">About</NavLink>
           </div>  
 
           <div className="nav-link-wrapper">
-            <NavLink to="/contact-me" activeClassName="active-nav-class">Contact Me</NavLink>
+            <NavLink to="/contact-me" activeClassName="active-nav-link">Contact Me</NavLink>
           </div>
 
           <div className="nav-link-wrapper">  
-            <NavLink to="/blog" activeClassName="active-nav-class">Blog</NavLink>
+            <NavLink to="/blog" activeClassName="active-nav-link">Blog</NavLink>
           </div>
         </div>
         <div className="right-side">Kage Luger</div>
