56e03383afe120d43621f9d199af93356f53afe5
Kage Luger <wu76iyqr2f@privaterelay.appleid.com>
2021-05-14 17:36:32 -0500

    sidebar-edit-style-update-not-functional

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,65 +1,80 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withRouter } from "react-router";
 //HOC are lowercaseToCapital
-import { NavLink } from 'react-router-dom';
-
-
+import { NavLink } from "react-router-dom";
 
 const NavigationComponent = (props) => {
-
-  const dynamicLink = ( route, linkText) => {
-    return(
-      <div className="nav-link-wrapper">  
-      <NavLink to={route} activeClassName="active-nav-link">{linkText}</NavLink>
+  const dynamicLink = (route, linkText) => {
+    return (
+      <div className="nav-link-wrapper">
+        <NavLink to={route} activeClassName="active-nav-link">
+          {linkText}
+        </NavLink>
       </div>
     );
   };
 
   const handleSignOut = () => {
-    axios.delete("https://api.devcamp.space/logout", {withCredentials: true}).then(response => {
-      if(response.status === 200 ) {
-        props.history.push("/");
-        props.handleSuccessfulLogout();
-      }
-      return response.data;
-    }).catch(error => {
-      console.log("error Siging out", error)
-    });
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then((response) => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("error Siging out", error);
+      });
   };
-  
 
-    return (
-      <div className="nav-wrapper">
-        
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="active-nav-link">Home</NavLink>
-          </div>
-
-          <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="active-nav-link">About</NavLink>
-          </div>  
-
-          <div className="nav-link-wrapper">
-            <NavLink to="/contact-me" activeClassName="active-nav-link">Contact Me</NavLink>
-          </div>
-
-          <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="active-nav-link">Blog</NavLink>
-          </div>
-        
-         { props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/portfolio-manager", "Portfolio Manager") : null}
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="active-nav-link">
+            Home
+          </NavLink>
         </div>
-        
-        <div className="right-side">
-          Kage Luger
-          {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="active-nav-link">
+            About
+          </NavLink>
         </div>
-      
-        {/* {true ? <button>Add Blog</button> : null } */}
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact-me" activeClassName="active-nav-link">
+            Contact Me
+          </NavLink>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="active-nav-link">
+            Blog
+          </NavLink>
+        </div>
+
+        {props.loggedInStatus === "LOGGED_IN"
+          ? dynamicLink("/portfolio-manager", "Portfolio Manager")
+          : null}
       </div>
-    );
-  }
+
+      <div className="right-side">
+        Kage Luger
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt"></FontAwesomeIcon>
+          </a>
+        ) : null}
+      </div>
+
+      {/* {true ? <button>Add Blog</button> : null } */}
+    </div>
+  );
+};
 
 export default withRouter(NavigationComponent);
--- src/components/pages/portfolio-mangager.js
+++ src/components/pages/portfolio-mangager.js
@@ -10,6 +10,7 @@ export default class PortfolioManager extends Component {
 
     this.state = {
       portfolioItems: [],
+      portfolioToEdit: {},
     };
 
     this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
@@ -17,6 +18,13 @@ export default class PortfolioManager extends Component {
     );
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick(portfolioItem) {
+    this.setState({
+      portfolioToEdit: portfolioItem,
+    });
   }
 
   handleDeleteClick(portfolioItem) {
@@ -88,8 +96,9 @@ export default class PortfolioManager extends Component {
         </div>
         <div className="right-column">
           <PortfolioSideBarList
-            data={this.state.portfolioItems}
             handleDeleteClick={this.handleDeleteClick}
+            data={this.state.portfolioItems}
+            handleEditClick={this.handleEditClick}
           />
         </div>
       </div>
